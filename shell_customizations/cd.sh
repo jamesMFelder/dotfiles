1cca603bcb82aa106d2a8bbd703ace51e408cb2c
James Felder <james.m.felder@gmail.com>
2020-10-27 21:32:21 -0400

    Install zsh.

--- shell_customizations/cd.sh
+++ shell_customizations/cd.sh
@@ -1,4 +1,4 @@
-#autocd
-shopt -s autocd
+#autocd if in bash
+[ "$0" = "/bin/bash" ] && shopt -s autocd
 #Assume I will oftem want to jump right into a src directory
 if test “${PS1+set}”; then CDPATH=".:/home/james/src/"; fi
--- shell_customizations/man.sh
+++ shell_customizations/man.sh
@@ -1,5 +1,5 @@
 #Colorize man output
-man() {
+function man () {
     export LESS_TERMCAP_md=$'\e[01;31m' \
     export LESS_TERMCAP_me=$'\e[0m' \
     export LESS_TERMCAP_us=$'\e[01;32m' \
