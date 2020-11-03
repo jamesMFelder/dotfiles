b23b3ddde89ebf11c69ba49f5d367b302f066895
James Felder <james.m.felder@gmail.com>
2020-11-03 13:41:21 -0500

    Revert "Hopefully this will make dash work."
    This reverts commit 96b2ac7e2e5a0530cdf27fa49ed9358af593e40a.

--- install.sh
+++ install.sh
@@ -50,11 +50,6 @@ printf "Linked %s to ...\n" "$HOME/.bash_aliases" "${PREFIX}/dotfiles/.bash_alia
 [ -f "$HOME/.zshrc" ] && mv -i "$HOME/.zshrc" "$HOME/.zshrc.bak" && printf "Moved %s to %s...\n" "$HOME/.zshrc" "$HOME/.zshrc.bak"
 ln -s "${PREFIX}/dotfiles/.zshrc" "$HOME/.zshrc"
 printf "Linked %s to %s...\n" "$HOME/.zshrc" "${PREFIX}/dotfiles/.zshrc"
-#Dash
-[ -f "$HOME/.dashrc" ] && mv -i "$HOME/.dashrc" "$HOME/.dashrc.bak" && printf "Moved %s to %s...\n" "$HOME/.dashrc" "$HOME/.dashrc.bak"
-ln -s "${PREFIX}/dotfiles/.dashrc" "$HOME/.dashrc"
-printf "Linked %s to %s...\n" "$HOME/.dashrc" "${PREFIX}/dotfiles/.dashrc"
-printf "Remember to add '%s' to your $HOME/.profile" "export ENV=$HOME/.dashrc"
 #Inputrc
 [ -f "$HOME/.inputrc" ] && mv -i "$HOME/.inputrc" "$HOME/.inputrc.bak" && printf "Moved %s to %s...\n" "$HOME/.inputrc" "$HOME/.inputrc.bak"
 ln -s "${PREFIX}/dotfiles/.inputrc" "$HOME/.inputrc"
