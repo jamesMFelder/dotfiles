6625ee90bf6042d461d845ce7130652b46cbc0fc
James Felder <james.m.felder@gmail.com>
2021-12-04 20:02:43 -0500

    Use vim as $MANPAGER

--- shell_customizations/man.sh
+++ shell_customizations/man.sh
@@ -1,2 +1,3 @@
-#Colorize man output
-export MANPAGER="less -R --use-color -Dd+r -Du+g"
+#Use vim as the pager for man
+#The set ft=man provides syntax highlighting and such
+export MANPAGER="/bin/sh -c \"col -b | vim -c 'set ft=man ts=8 nomod nolist nonu noma' - \""
