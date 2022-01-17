87c2fb1d06ab3d250426822d0fb4b90bd0beaaff
James Felder <james.m.felder@gmail.com>
2022-01-17 06:07:08 -0500

    Make the `,` alias take input from stdin rather than it's arguments.

--- shell_customizations/numbers.sh
+++ shell_customizations/numbers.sh
@@ -1,3 +1,3 @@
 #Put commas in big numbers (the rev command reverses the lines of it's input)
 #And [[:digit:]] matches 0-9 (TODO: xdigit to match hexadacimal?)
-alias ,="{ rev | sed 's/[[:digit:]][[:digit:]][[:digit:]]/&,/g' | rev; } <<< "
+alias ,="{ rev | sed 's/[[:digit:]]\{3\}/&,/g' | rev; }"
