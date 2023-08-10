66437fd9cd0015cbfa5d349b7378e53ddb3eaa94
James McNaughton Felder <james.m.felder@gmail.com>
2023-08-10 12:24:46 -0400

    Add function rmcurdir and fix mkcd
    Use -p flag for mkdir to allow creating multiple levels
    rmcurdir removes the current directory if it is empty (rmdir), leaving
    you outside of it

--- shell_customizations/cd.sh
+++ shell_customizations/cd.sh
@@ -1,5 +1,12 @@
 #Create a directory and enter into it
 function mkcd(){
 	#shellcheck disable=2164 #We don't do anything after the `cd` so we don't handle an error from it.
-	mkdir "$1" && cd "$1"
+	mkdir -p "$1" && cd "$1"
+}
+
+#Remove the current directory if empty (leave it first)
+function rmcurdir(){
+	local curdir="${PWD}"
+	cd ..
+	rmdir "${curdir}"
 }
