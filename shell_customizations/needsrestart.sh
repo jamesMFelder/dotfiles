3f24f203b20f987c6a2bc7063249294014ebbd82
James McNaughton Felder <james.m.felder@gmail.com>
2023-08-10 12:47:41 -0400

    Add a stripped-down version of the needsrestart script

---
+++ shell_customizations/needsrestart.sh
@@ -0,0 +1,21 @@
+# List what processes are depending on what deleted files
+
+# Prints the command line, and the file
+# stdin should be formatted as multiple lines,
+# each one containing "/proc/pid/cmdline file"
+function print_pid_and_file() {
+	while read -r pid file; do
+		cmdline=$(tr '\0' ' ' < "$pid")
+		printf '"%s" → "%s"\n' "$cmdline" "$file"
+	done
+}
+
+# Prints all the currently running processes depending on missing files
+function needsrestart() {
+	#shellcheck disable=2016 # we actually don't want to expand the command within the sed expression until we have the correct arguments for it
+	grep -F '(deleted)' /proc/*/maps 2>/dev/null | # Get the processes
+		awk '{print $1,$6}' | sed 's/:.* / /' | # Filter the output to only get "/proc/$pid/maps $file"
+		grep -v '/memfd\|/SYSV\|/dev/shm' | # Ignore false positives (non-files)
+		sed 's@/proc/\([[:digit:]]\+\)/maps \(.*\)@/proc/\1/cmdline \2@' | # We care about the /proc/pid/cmdline, not /proc/pid/maps
+		print_pid_and_file # Actually print everything
+}
