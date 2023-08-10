5c05ce261bafd8a4f16ff89ea5431a8b8f44afc0
James McNaughton Felder <james.m.felder@gmail.com>
2023-08-10 12:27:18 -0400

    Set $HISTCONTROL

--- shell_customizations/history.sh
+++ shell_customizations/history.sh
@@ -2,3 +2,7 @@
 #Needs to match the full line (no implicit .* at the end)
 #      `ls dir/` is remembered, but `ls` isn't
 export HISTIGNORE="$HISTIGNORE:ls:clear:cls:exit:\:q"
+
+#Don't save lines starting with a space or
+#that are the same as the one above them
+export HISTCONTROL="ignoreboth"
