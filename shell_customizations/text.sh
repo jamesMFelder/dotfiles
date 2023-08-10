249c1aba16fa9a9622ccae42962f90055012348d
James McNaughton Felder <james.m.felder@gmail.com>
2023-08-10 12:31:01 -0400

    Add alias to get filter for a unicode character
    Uses a personal list of ones I most want - make the list you want

--- shell_customizations/text.sh
+++ shell_customizations/text.sh
@@ -1,3 +1,5 @@
 #Simple rot13 (en|de)coder
 #To use, echo "secret text" | rot13 or echo "frperg grkg" | rot13
 alias rot13='tr a-zA-Z n-za-mN-ZA-M'
+#Get the unicode character from ~/Documents/Notes/Unicode.txt
+alias unicode='ugrep -iQ $HOME/Documents/Notes/Unicode.txt | cut -d" " -f1 | tr -d "\n" | wl-copy'
