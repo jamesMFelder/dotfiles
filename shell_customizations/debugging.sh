21bc10b487494853b12004d01bffce5a468b7b9b
James McNaughton Felder <james.m.felder@gmail.com>
2024-12-19 10:24:15 -0500

    Add x86_64 disassemble alias

--- shell_customizations/debugging.sh
+++ shell_customizations/debugging.sh
@@ -1,3 +1,4 @@
 alias disassemble='objdump -C -w -d -S -l --visualize-jumps=color --disassembler-color=color'
 alias i686-elf-disassemble='i686-elf-objdump -C -w -d -S -l --visualize-jumps=color --disassembler-color=color'
+alias x86_64-elf-disassemble='x86_64-elf-objdump -C -w -d -S -l --visualize-jumps=color --disassembler-color=color'
 alias arm-none-eabi-disassemble='arm-none-eabi-objdump -C -w -d -S -l --visualize-jumps=color --disassembler-color=color'
